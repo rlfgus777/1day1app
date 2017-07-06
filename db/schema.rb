4866e1283ccd7c5a073a2f690bee3a2006f33e8a
Gilhyeon Gim <rlfgus777@gmail.com>
2017-07-06 07:45:10 +0000

    comment model 만듦

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706072341) do
+ActiveRecord::Schema.define(version: 20170706074307) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "memo_id"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["memo_id"], name: "index_comments_on_memo_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "memos", force: :cascade do |t|
     t.string   "title"
