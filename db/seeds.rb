81e44d0c764af21fc7cba3c8b333cd27111fbc6d
Gilhyeon Gim <rlfgus777@gmail.com>
2017-07-06 09:40:40 +0000

    git faker 사용 seed 사용

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |i|
+  if i%2 == 0
+    user = 1
+  else
+    user = 2
+  end
+  Memo.create!(title: "#{i+1} 번째 글이다 이야이야호", content: Faker::Lorem.paragraph(10), user_id: user)
+end
